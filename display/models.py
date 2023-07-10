431e2b968dc6d800c03f36b1dc2b69422688faf0
Marcelo <marcelodr99@gmail.com>
2023-07-10 17:08:53 -0400

    updated models for display

--- display/models.py
+++ display/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
 
 class Display(models.Model):
     message = models.TextField(blank=True)
     msg_date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
